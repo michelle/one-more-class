c592d7ba183cec2dfaa127413889a451dd7491b5
Michelle Bu <analogmidnight@gmail.com>
2012-10-19 22:13:40 -0700

    finished

--- server.js
+++ server.js
@@ -9,18 +9,20 @@ var options = {
     html: secret.myCCN
 };
 
-var myCheck = /0 student\(s\) are enrolled, with a limit of 180 and 1 student\(s\) are on the waiting list, with a limit of 1/;
+var myCheck = /0 student\(s\) are enrolled, with a limit of 180\n[\w\s]*1 student\(s\) are on the waiting list, with a limit of 1\n/;
+
 
 function spamScheduleBerkeley() {
+	console.log('I want to graduate :(');
 	rest.post('https://telebears.berkeley.edu/enrollment-osoc/osc', 
 		{ data: { '_InField1': 'RESTRIC',
 				  '_InField2': secret.myCCN,
 				  '_InField3': '13B4' }
 		}).on('complete', function(data, res) {
-			console.log(data, res);
-			/*if (!myCheck.test(res)) {
+			if (!myCheck.test(res.rawEncoded)) {
 				sendMail();
-			}*/
+				it += 1;
+			}
 		});
 	
 };
