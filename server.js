d56d126541512e9f008e89c59da8b70f75aa7586
Michelle Bu <analogmidnight@gmail.com>
2012-10-20 16:58:55 -0700

    fix tabbing

--- server.js
+++ server.js
@@ -1,51 +1,60 @@
 var secret = require('./secret.js');
 var rest = require('restler');
+var nodemailer = require('nodemailer');
+
+// create reusable transport method (opens pool of SMTP connections)
+var smtpTransport = nodemailer.createTransport('SMTP', {
+  service: 'Gmail',
+  auth: {
+    user: 'analogmidnight@gmail.com',
+    pass: secret.myPassword
+  }
+});
 
 var phoneOptions = {
-    from: 'analogmidnight@gmail.com',
-    to: secret.myNumber,
-    subject: 'ENROLL NOW',
-    text: secret.myCCN,
-    html: secret.myCCN
+  from: 'analogmidnight@gmail.com',
+  to: secret.myNumber,
+  subject: 'ENROLL NOW',
+  text: secret.myCCN,
+  html: secret.myCCN
 };
 
 var emailOptions = {
-    from: 'analogmidnight@gmail.com',
-    to: 'analogmidnight@gmail.com',
-    subject: 'ENROLL NOW',
-    text: secret.myCCN,
-    html: secret.myCCN
+  from: 'analogmidnight@gmail.com',
+  to: 'analogmidnight@gmail.com',
+  subject: 'ENROLL NOW',
+  text: secret.myCCN,
+  html: secret.myCCN
 };
 
 var myCheck = /0 student\(s\) are enrolled, with a limit of 180\n[\w\s]*1 student\(s\) are on the waiting list, with a limit of 1\n/;
 
 
 function spamScheduleBerkeley() {
-	console.log('I want to graduate :(');
-	rest.post('https://telebears.berkeley.edu/enrollment-osoc/osc', 
-		{ data: { '_InField1': 'RESTRIC',
-				  '_InField2': secret.myCCN,
-				  '_InField3': '13B4' }
-		}).on('complete', function(data, res) {
-			if (data && !myCheck.test(data)) {
-			  emailOptions.text = data;
-			  emailOptions.html = data;
-				sendMail(phoneOptions);
-				sendMail(emailOptions);
-			}
-		});
-	
+  console.log('I want to graduate :(');
+  rest.post('https://telebears.berkeley.edu/enrollment-osoc/osc', 
+    { data: { '_InField1': 'RESTRIC',
+              '_InField2': secret.myCCN,
+              '_InField3': '13B4' }
+    }).on('complete', function(data, res) {
+      if (data && !myCheck.test(data)) {
+        emailOptions.text = data;
+        emailOptions.html = data;
+        sendMail(phoneOptions);
+        sendMail(emailOptions);
+      }
+    });
 };
 
 
 function sendMail(options) {
-	secret.smtpTransport.sendMail(options, function(err, res){
-    	if (err) {
-        	console.log(err);
-    	} else {
-        	console.log(res.message);
-    	}
-	});
+  smtpTransport.sendMail(options, function(err, res){
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(res.message);
+    }
+  });
 };
 
 
